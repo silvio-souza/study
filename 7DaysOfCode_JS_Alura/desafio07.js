b546b8942207b8a0960f41f1fc0a2e0c14f44e15
silvio-souza <sjblu7@gmail.com>
2023-08-07 17:31:47 -0400

    challenge07 completed

--- 7DaysOfCode_JS_Alura/desafio07.js
+++ 7DaysOfCode_JS_Alura/desafio07.js
@@ -1,4 +1,5 @@
 const { Console } = require("console");
+const { read } = require("fs");
 
 const readline = require("readline").createInterface({
   input: process.stdin,
@@ -8,54 +9,149 @@ const readline = require("readline").createInterface({
 console.log("-----CALCULADORA-----");
 
 const perguntaInicial = () => {
-  console.log("Informe a operacao que deseja efetuar");
+  console.log("\nInforme a operacao que deseja efetuar");
   readline.question(
-    "\nEssas são as opcoes: SOMA, SUBTRACAO, MULTIPLICACAO, DIVISAO ou SAIR",
+    "Essas são as opcoes: SOMA, SUBTRACAO, MULTIPLICACAO, DIVISAO ou SAIR\n",
     (operacao) => {
-        switch(operacao.toLocaleLowerCase()) {
-            case "soma":
-                somar();
-                break;
+      switch (operacao.toLocaleLowerCase()) {
+        case "soma":
+          somar();
+          break;
 
-            case "subtracao":
-                subtrair();
-                break;
+        case "subtracao":
+          subtrair();
+          break;
 
-            case "multiplicacao":
-                multiplicar();
-                break;
-            
-            case "divisao":
-                dividir();
-                break;
+        case "multiplicacao":
+          multiplicar();
+          break;
 
-            case "sair":
-                console.log("Ate a proxima!");
-                readline.close;
+        case "divisao":
+          dividir();
+          break;
 
-            default:
-                console.log("Opcao nao encontrada. Tente de novo.");
-        }   
-        perguntaInicial();
+        case "sair":
+          console.log("\nAte a proxima!\n");
+          readline.close();
+          break;
+
+        default:
+          console.log("Opcao nao encontrada. Tente de novo.");
+          perguntaInicial();
+      }
     }
   );
 };
 
-
 const somar = () => {
-
-}
+  const operacao = () => {
+    console.log("\nInforme os numeros que deseja somar");
+    readline.question("Primeiro numero: ", (num1) => {
+      if (isNaN(Number(num1))) {
+        console.log(
+          `O caracter informado "${num1}" nao e um numero. Tente novamente`
+        );
+        operacao();
+      } else {
+        readline.question("Segundo numero: ", (num2) => {
+          if (isNaN(Number(num2))) {
+            console.log(
+              `O caracter informado "${num2}" nao e um numero. Tente novamente`
+            );
+            operacao();
+          } else {
+            console.log(`${num1} + ${num2} = ${Number(num1) + Number(num2)}`);
+            perguntaInicial();
+          }
+        });
+      }
+    });
+  };
+  operacao();
+};
 
 const subtrair = () => {
-    
-}
+  const operacao = () => {
+    console.log("\nInforme os numeros que deseja subtrair");
+    readline.question("Primeiro numero: ", (num1) => {
+      if (isNaN(Number(num1))) {
+        console.log(
+          `O caracter informado "${num1}" nao e um numero. Tente novamente`
+        );
+        operacao();
+      } else {
+        readline.question("Segundo numero: ", (num2) => {
+          if (isNaN(Number(num2))) {
+            console.log(
+              `O caracter informado "${num2}" nao e um numero. Tente novamente`
+            );
+            operacao();
+          } else {
+            console.log(`${num1} - ${num2} = ${Number(num1) - Number(num2)}`);
+            perguntaInicial();
+          }
+        });
+      }
+    });
+  };
+  operacao();
+};
 
 const multiplicar = () => {
-    
-}
+  const operacao = () => {
+    console.log("\nInforme os numeros que deseja multiplicar");
+    readline.question("Primeiro numero: ", (num1) => {
+      if (isNaN(Number(num1))) {
+        console.log(
+          `O caracter informado "${num1}" nao e um numero. Tente novamente`
+        );
+        operacao();
+      } else {
+        readline.question("Segundo numero: ", (num2) => {
+          if (isNaN(Number(num2))) {
+            console.log(
+              `O caracter informado "${num2}" nao e um numero. Tente novamente`
+            );
+            operacao();
+          } else {
+            console.log(`${num1} x ${num2} = ${Number(num1) * Number(num2)}`);
+            perguntaInicial();
+          }
+        });
+      }
+    });
+  };
+  operacao();
+};
 
 const dividir = () => {
-    
-}
+  const operacao = () => {
+    console.log("\nInforme os numeros que deseja dividir");
+    readline.question("Primeiro numero: ", (num1) => {
+      if (isNaN(Number(num1))) {
+        console.log(
+          `O caracter informado "${num1}" nao e um numero. Tente novamente`
+        );
+        operacao();
+      } else {
+        readline.question("Segundo numero: ", (num2) => {
+          if (isNaN(Number(num2))) {
+            console.log(
+              `O caracter informado "${num2}" nao e um numero. Tente novamente`
+            );
+            operacao();
+          } else if (num2 == 0) {
+            console.log("Nao pode dividir por zero.");
+            operacao();
+          } else {
+            console.log(`${num1} / ${num2} = ${Number(num1) / Number(num2)}`);
+            perguntaInicial();
+          }
+        });
+      }
+    });
+  };
+  operacao();
+};
 
 perguntaInicial();
